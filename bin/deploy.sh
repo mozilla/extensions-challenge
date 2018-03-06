9d8950a3142cb0c7fed180954adb501076411098
Stuart Colville <scolville@mozilla.com>
2018-03-06 19:06:22 +0000

    Add initial build script

---
+++ bin/deploy.sh
@@ -0,0 +1,106 @@
+i#!/bin/bash
+
+# This file is used to deploy Extensions Challenge to an S3 bucket. It
+# expects to be run from the root of the extensions-challenge directory and
+# you'll need your S3 bucket name in an environment variable
+# $EXTCHALLENGE_BUCKET
+
+if [ ! -d "public" ]; then
+    echo "Can't find /public/ directory.  Are you running from the extensions-challenge?"
+    exit 1
+fi
+
+if [ -z "$EXTCHALLENGE_BUCKET" ]; then
+    echo "The S3 bucket is not set. Failing."
+    exit 1
+fi
+
+# The basic strategy is to sync all the files that need special attention
+# first, and then sync everything else which will get defaults
+
+
+# For short-lived assets; in seconds
+TEN_MINUTES="600"
+
+# For long-lived assets; in seconds
+ONE_YEAR="31536000"
+
+CSP="\"content-security-policy\": \"default-src 'self'; font-src 'self' code.cdn.mozilla.net; form-action 'none'; frame-ancestors 'self'; img-src 'self' https://ssl.google-analytics.com https://www.google-analytics.com; object-src 'none'; script-src 'self' https://ssl.google-analytics.com; style-src 'self'; report-uri /__cspreport__; frame-src 'none';\""
+HSTS="\"strict-transport-security\": \"max-age=${ONE_YEAR}; includeSubDomains; preload\""
+TYPE="\"x-content-type-options\": \"nosniff\""
+XSS="\"x-xss-protection\": \"1; mode=block\""
+ACAO="\"Access-Control-Allow-Origin\": \"*\""
+
+
+# build version.json if it isn't provided
+[ -e version.json ] || $(dirname $0)/build-version-json.sh
+
+if [ -e version.json ]; then
+    mv version.json public/__version__
+    # __version__ JSON; short cache
+    aws s3 cp \
+      --cache-control "max-age=${TEN_MINUTES}" \
+      --content-type "application/json" \
+      --metadata "{${ACAO}, ${HSTS}, ${TYPE}}" \
+      --metadata-directive "REPLACE" \
+      --acl "public-read" \
+      public/__version__ s3://${EXTCHALLENGE_BUCKET}/__version__
+fi
+
+# HTML; short cache
+aws s3 sync \
+  --cache-control "max-age=${TEN_MINUTES}" \
+  --content-type "text/html" \
+  --exclude "*" \
+  --include "*.html" \
+  --metadata "${CSP}, ${HSTS}, ${TYPE}, ${XSS}}" \
+  --metadata-directive "REPLACE" \
+  --acl "public-read" \
+  public/ s3://${EXTCHALLENGE_BUCKET}/
+
+# JSON; short cache
+aws s3 sync \
+  --cache-control "max-age=${TEN_MINUTES}" \
+  --content-type "application/json" \
+  --exclude "*" \
+  --include "*.json" \
+  --metadata "{${ACAO}, ${HPKP}, ${HSTS}, ${TYPE}}" \
+  --metadata-directive "REPLACE" \
+  --acl "public-read" \
+  public/ s3://${EXTCHALLENGE_BUCKET}/
+
+# SVG; cache forever, assign correct content-type
+aws s3 sync \
+  --cache-control "max-age=${ONE_YEAR}, immutable" \
+  --content-type "image/svg+xml" \
+  --exclude "*" \
+  --include "*.svg" \
+  --metadata "${HSTS}, ${TYPE}}" \
+  --metadata-directive "REPLACE" \
+  --acl "public-read" \
+  public/ s3://${EXTCHALLENGE_BUCKET}/
+
+# Everything else; cache forever, because it has hashes in the filenames
+aws s3 sync \
+  --delete \
+  --cache-control "max-age=${ONE_YEAR}, immutable" \
+  --metadata "${HSTS}, ${TYPE}}" \
+  --metadata-directive "REPLACE" \
+  --acl "public-read" \
+  public/ s3://${EXTCHALLENGE_BUCKET}/
+
+# HTML - `path/index.html` to `path` resources; short cache
+for fn in $(find public -name 'index.html' -not -path 'public/index.html'); do
+  s3path=${fn#public/}
+  s3path=${s3path%/index.html}
+  aws s3 cp \
+    --cache-control "max-age=${TEN_MINUTES}" \
+    --content-type "text/html" \
+    --exclude "*" \
+    --include "*.html" \
+    --metadata "${CSP}, ${HSTS}, ${TYPE}, ${XSS}}" \
+    --metadata-directive "REPLACE" \
+    --acl "public-read" \
+    $fn s3://${EXTCHALLENGE_BUCKET}/${s3path}
+done
+
