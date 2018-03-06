be9d1c61508134a4929deaf88884e190375d9249
bqbn <bqbn@openken.com>
2018-03-06 13:26:00 -0800

    Make DISTRIBUTION_ID an environment variable
    This made it easy to change the distribution id when needed. It also
    made it consistent with the S3 bucket being an environment variable.

--- bin/circleci/invalidate-cloudfront-cache.sh
+++ bin/circleci/invalidate-cloudfront-cache.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ex
 
-DISTRIBUTION_ID=$1
+if [ -z "$DISTRIBUTION_ID" ]; then
+    echo "The Cloudfront distribution id is not set. Failing."
+    exit 1
+fi
 
 aws configure set preview.cloudfront true
 
