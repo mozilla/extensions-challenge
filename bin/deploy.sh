6bbe1b1692eca215e4dd6019507659a0082f7565
Stuart Colville <scolville@mozilla.com>
2018-03-06 19:22:22 +0000

    Update CSP (fixes #5)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -25,7 +25,7 @@ TEN_MINUTES="600"
 # For long-lived assets; in seconds
 ONE_YEAR="31536000"
 
-CSP="\"content-security-policy\": \"default-src 'self'; font-src 'self' code.cdn.mozilla.net; form-action 'none'; frame-ancestors 'self'; img-src 'self' https://ssl.google-analytics.com https://www.google-analytics.com; object-src 'none'; script-src 'self' https://ssl.google-analytics.com; style-src 'self'; report-uri /__cspreport__; frame-src 'none';\""
+CSP="\"content-security-policy\": \"default-src 'self'; font-src 'self' fonts.gstatic.com use.typekit.net; form-action 'none'; frame-ancestors 'self'; img-src 'self' https://ssl.google-analytics.com https://www.google-analytics.com; object-src 'none'; script-src 'self' use.typekit.net https://ssl.google-analytics.com; style-src 'self' fonts.googleapis.com; report-uri /__cspreport__; frame-src 'none';\""
 HSTS="\"strict-transport-security\": \"max-age=${ONE_YEAR}; includeSubDomains; preload\""
 TYPE="\"x-content-type-options\": \"nosniff\""
 XSS="\"x-xss-protection\": \"1; mode=block\""
