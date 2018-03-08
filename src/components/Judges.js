ab4aad1744038afe13dfb496717331edb3146126
Stuart Colville <scolville@mozilla.com>
2018-03-08 14:43:49 +0000

    Add scrollable fragment ids to prizes and judges

--- src/components/Judges.js
+++ src/components/Judges.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
 
 // images
 import mike from './img/judges/mike.png';
@@ -7,6 +8,8 @@ import bianca from './img/judges/bianca.png';
 import jyotsna from './img/judges/jyotsna.png';
 import swarnava from './img/judges/swarnava.png';
 import jorge from './img/judges/jorge.png';
+
+configureAnchors({ offset: -90 });
 
 const Judge = ({
   image, name, desc, link,
@@ -21,47 +24,50 @@ const Judge = ({
 );
 
 const Judges = () => (
-  <div className="judges">
-    <div className="container">
-      <h2>Judges</h2>
-      <p className="pt4 px4">
-        A panel of judges will select the finalists for each prize category, and then the community
-        will vote to decide the winners of each category.
-      </p>
-      <div className="judges__wrap">
-        <Judge
-          image={mike}
-          name="Mike Conca"
-          desc="WebExtensions APIs Product Manager"
-          link="https://mozillians.org/u/mconca"
-        />
-        <Judge
-          image={bianca}
-          name="Bianca Danforth"
-          desc="Firefox Front End Experiments Engineer"
-          link="https://mozillians.org/u/biancadanforth/"
-        />
-        <Judge
-          image={jyotsna}
-          name="Jyotsna Gupta"
-          desc="Extension Developer"
-          link="https://mozillians.org/u/jyotsna17gupta/"
-        />
-        <Judge
-          image={swarnava}
-          name="Swarnava Sengupta"
-          desc="Featured Add-ons Advisory Board Lead"
-          link="https://mozillians.org/u/Swarnava/"
-        />
-        <Judge
-          image={jorge}
-          name="Jorge Villalobos"
-          desc="AMO Product Manager"
-          link="https://mozillians.org/u/jorgev"
-        />
+
+  <ScrollableAnchor id="judges">
+    <div className="judges">
+      <div className="container">
+        <h2>Judges</h2>
+        <p className="pt4 px4">
+          A panel of judges will select the finalists for each prize category, and then
+          the community will vote to decide the winners of each category.
+        </p>
+        <div className="judges__wrap">
+          <Judge
+            image={mike}
+            name="Mike Conca"
+            desc="WebExtensions APIs Product Manager"
+            link="https://mozillians.org/u/mconca"
+          />
+          <Judge
+            image={bianca}
+            name="Bianca Danforth"
+            desc="Firefox Front End Experiments Engineer"
+            link="https://mozillians.org/u/biancadanforth/"
+          />
+          <Judge
+            image={jyotsna}
+            name="Jyotsna Gupta"
+            desc="Extension Developer"
+            link="https://mozillians.org/u/jyotsna17gupta/"
+          />
+          <Judge
+            image={swarnava}
+            name="Swarnava Sengupta"
+            desc="Featured Add-ons Advisory Board Lead"
+            link="https://mozillians.org/u/Swarnava/"
+          />
+          <Judge
+            image={jorge}
+            name="Jorge Villalobos"
+            desc="AMO Product Manager"
+            link="https://mozillians.org/u/jorgev"
+          />
+        </div>
       </div>
     </div>
-  </div>
+  </ScrollableAnchor>
 );
 
 Judge.propTypes = {
--- src/components/Prizes.js
+++ src/components/Prizes.js
@@ -1,29 +1,34 @@
 import React from 'react';
 
+import ScrollableAnchor, { configureAnchors } from 'react-scrollable-anchor';
+
 // images
 import prize1 from './img/prize1.png';
 import prize2 from './img/prize2.png';
 
-const Prizes = () => (
-  <div className="prizes">
-    <div className="container">
-      <h2>Prizes</h2>
+configureAnchors({ offset: -90 });
 
-      <div className="prizes__list">
-        <div className="prizes__item">
-          <img src={prize1} alt="iPad Pro" />
-          <p>
-            Category winners will receive an Apple iPad Pro 10.5” Wifi 256GB and be featured on
-            addons.mozilla.org.
-          </p>
-        </div>
-        <div className="prizes__item">
-          <img src={prize2} alt="Amazon Gift Card" />
-          <p>Runners up in each category will receive a $250 USD Amazon gift card.</p>
+const Prizes = () => (
+  <ScrollableAnchor id="prizes">
+    <div className="prizes">
+      <div className="container">
+        <h2>Prizes</h2>
+        <div className="prizes__list">
+          <div className="prizes__item">
+            <img src={prize1} alt="iPad Pro" />
+            <p>
+              Category winners will receive an Apple iPad Pro 10.5” Wifi 256GB and be featured on
+              addons.mozilla.org.
+            </p>
+          </div>
+          <div className="prizes__item">
+            <img src={prize2} alt="Amazon Gift Card" />
+            <p>Runners up in each category will receive a $250 USD Amazon gift card.</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </ScrollableAnchor>
 );
 
 export default Prizes;
