6bb00f84e55e1f81c92c7623e7d417ce4be4743d
Dan Brown <slightlyoffbeat@gmail.com>
2018-05-08 11:18:39 -0700

    minor welogo css changes

--- src/components/Section2.js
+++ src/components/Section2.js
@@ -8,7 +8,14 @@ import wetransfer from './img/wetransfer.svg';
 const Section2 = () => (
   <div className="section2">
     <div className="container submit__grid">
-      <img className="hero__we-logo" src={wetransfer} alt="WeTransfer" />
+      <a
+        className="hero__we-logo"
+        href="https://wetransfer.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img className="" src={wetransfer} alt="WeTransfer" />
+      </a>
       <p className="hero__we-text">
         The Firefox Quantum Extensions Challenge is supported by WeTransfer, who is donating a
         1-year WeTransfer Plus account for its file transfer and storage service to eligible
